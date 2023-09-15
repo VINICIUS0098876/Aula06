8bb77935dee8cac6f5ba257eff352210721a1212
VINICIUS0098876 <124823788+VINICIUS0098876@users.noreply.github.com>
2023-09-15 17:17:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+async function pegarImagens(){
+    const endPoint = 'https://dog.ceo/api/breed/doberman/images'
+    const response = await fetch(endPoint)
+    const imagens = await response.json()
+    return imagens.message
+
+}
+
+function criarTagImg(imagem){
+    const galeria = document.getElementById('galeria')
+    const tagImg = document.createElement('img')
+    tagImg.src = imagem
+    galeria.appendChild(tagImg)
+}
+
+async function carregarFotos(){
+    const imagens = await pegarImagens()
+    imagens.forEach(criarTagImg)
+}
+
+carregarFotos()
